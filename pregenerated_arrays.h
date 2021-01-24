18a2fbdeff64dd2a7ec23f970551099b9793b457
alxrMironov <330OMcorporative>
2021-01-24 11:29:53 +0300

    Initial

---
+++ pregenerated_arrays.h
@@ -0,0 +1,22 @@
+#ifndef PREGENERATED_ARRAYS_
+#define PREGENERATED_ARRAYS_
+
+	#include <stdbool.h>
+	#include <stdint.h>
+
+	extern const uint16_t U8_MUL10[256];
+	extern const int16_t S8_MUL10[256];
+	
+	extern const uint8_t U8_DIV10[256];
+	extern const int8_t S8_DIV10[256];
+	
+	extern const uint8_t U8_MOD10[256];
+	extern const int8_t S8_MOD10[256];
+	
+	extern const char NIBBLE_TO_CHAR[16];
+
+	extern const uint8_t PERCENT_TO_BYTE[101];
+	extern const uint8_t PERCENT_TO_5BIT[101];
+	extern const uint8_t BYTE_TO_PERCENT[256];
+	
+#endif 
